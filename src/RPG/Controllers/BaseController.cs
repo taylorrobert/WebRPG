5b5c87adb5571b80ceac2b2beb6881a2e39e0b66
taylorhill89 <taylorhill@gmail.com>
2015-12-10 13:16:45 -0800

    changed course

--- src/RPG/Controllers/BaseController.cs
+++ src/RPG/Controllers/BaseController.cs
@@ -2,23 +2,37 @@
 using System.Security.Claims;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Filters;
+using Microsoft.AspNet.Routing;
 using RPG.Models;
 
 namespace RPG.Controllers
 {
     public class BaseController : Controller
     {
-        public ApplicationDbContext _context;
+        public ApplicationDbContext db;
         public ApplicationUser _applicationUser;
+        public Corporation _corporation;
 
         public BaseController(ApplicationDbContext context)
         {
-            _context = context;
+            db = context;
         }
 
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            _applicationUser = _context.Users.FirstOrDefault(u => u.Id == User.GetUserId());
+            _applicationUser = db.Users.FirstOrDefault(u => u.Id == User.GetUserId());
+
+            if (_applicationUser != null)
+            {
+                _corporation = db.Corporations.FirstOrDefault(c => c.User.Id == _applicationUser.Id);
+                if (_corporation == null)
+                    context.Result = new RedirectToRouteResult(
+                        new RouteValueDictionary
+                        {
+                            {"Controller", "Corporations"},
+                            {"Action", "Create"}
+                        });
+            }
             base.OnActionExecuting(context);
         }
     }
